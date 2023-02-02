1a0f93d1940dd8dca6dce21e9c8c26187c2c4aa0
Blaser993 <corradodepinto.design@gmail.com>
2023-02-02 15:10:15 +0100

    FEAT  ciclo "for" che scrive i numeri da 1 a 100

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("ciao")
+console.log("ciao");
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi (quindi multipli di 15) “FizzBuzz”.
+
+for (let i = 1; i <= 100; i++) {
+    
+    let number = i;
+    console.log(number)
+
+}
